139d08dac08c45396ad080bc86f3be744b3263ba
Sonylal <sonylaledp@gmail.com>
2021-12-16 09:08:47 +0530

    changes made

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  user: any;
+  userData: any;
 
-  constructor() { }
+
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+    this.userData = JSON.parse(sessionStorage.getItem('userData') || '{}');
+    console.log(this.userData);
+    this.user = {
+      userName: '',
+      password: '',
+    }
   }
 
+  login(){
+    console.log(this.user)
+    if(this.user.username === this.userData.username && this.user.password === this.userData.password){
+      this.route.navigate(['/']);
+    } else {
+      alert("Incorrect username/password");
+    }
+  }
 }
